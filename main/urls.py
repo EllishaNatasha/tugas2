cda3f22266ee4cfd196c83c6fc9491285b645a19
EllishaNatasha <ellishanatasha28@gmail.com>
2023-11-22 11:37:15 +0700

    tugas 9 selesai

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_product, show_html, show_xml, show_json, show_xml_by_id, show_json_by_id
+from main.views import create_item_flutter, show_main, create_product, show_html, show_xml, show_json, show_xml_by_id, show_json_by_id
 from main.views import register, login_user, logout_user
 from main.views import get_item_json, add_item_ajax, delete_item
 
@@ -18,5 +18,6 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('get-item/', get_item_json, name='get_item_json'),
     path('create-item-ajax/', add_item_ajax, name='add_item_ajax'),
-    path('delete_item/<int:id>/', delete_item, name='delete_item')
+    path('delete_item/<int:id>/', delete_item, name='delete_item'),
+    path('create-flutter/', create_item_flutter, name='create_product_flutter'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
+import json
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponseNotFound
+from django.http import HttpResponseRedirect, HttpResponseNotFound, JsonResponse
 from main.forms import ProductForm
 from django.urls import reverse
 from .models import Item
@@ -121,3 +122,22 @@ def delete_item(request, id):
             item.delete()
             return HttpResponse(b"CREATED", status=201)
     return HttpResponseNotFound()
+
+@csrf_exempt
+def create_item_flutter(request):
+    if request.method == 'POST':
+        
+        data = json.loads(request.body)
+
+        new_item = Item.objects.create(
+            user = request.user,
+            name = data["name"],
+            amount = int(data["amount"]),
+            description = data["description"],
+        )
+
+        new_item.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
